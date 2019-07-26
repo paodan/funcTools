d11867f3e486efb3bcdea8931466e9453a2ef34b
paodan <weiyangtao1513@gmail.com>
2019-07-26 15:41:22 +0200

    add fastq2dataframe and fastq2dataframe_rev functions

---
+++ R/fastq2datatable.R
@@ -0,0 +1,59 @@
+#' Read fastq file and convert to a data frame
+#' @param fastqFile a fastq file name
+#' @import data.table
+#' @export
+#' @seealso \link{\code{fastq2dataframe_rev}}
+fastq2dataframe = function(fastqFile){
+  fastq = fread(file = fastqFile, sep = "\t", header = F)
+  n = nrow(fastq)
+  if (n %% 4 != 0){
+    stop("The row number of fastqFile is not multiple of 4, please check if the file is correct.")
+  }
+
+  fastq_table = data.frame(label = fastq$V1[seq(from = 1, to = n, by = 4)],
+                           sequence = fastq$V1[seq(from = 2, to = n, by = 4)],
+                           mark = fastq$V1[seq(from = 3, to = n, by = 4)],
+                           score = fastq$V1[seq(from = 4, to = n, by = 4)],
+                           stringsAsFactors = F)
+  return(fastq_table)
+}
+
+#' reverse a fastq corresponding data frame to a data.table of fastq format, and save a fastq file
+#' @param fastqDataframe a data frame of four columns, and the column names must be
+#' "label", "sequence", "mark", and "score".
+#' @param fileName the fastq file to be written.
+#' @import data.table
+#' @export
+#' @seealso \link{\code{fastq2dataframe}}
+#' @examples {
+#' \dontrun{
+#' # Generating information for 3 reads, of which is length of 50.
+#' len = 50
+#' n = 3
+#' sequences = sapply(1:n, function(x) paste0(sample(c("A", "T", "C", "G"), len, replace = T), collapse = ""))
+#' base = 33:(33+42) # ASCII_BASE=33
+#' mode(base) = "raw"
+#' scores = sapply(1:n, function(x) paste0(sapply(sample(base[2:40], len, replace = T), rawToChar), collapse = ""))
+#' fastq_test = data.frame(label = paste0("sequence", 1:n),
+#'                         sequence = sequences,
+#'                         mark = "+", score = scores)
+#' # save file
+#' fastq2dataframe_rev(fastq_test, "./fastq_test.fastq")
+#' }
+#' }
+fastq2dataframe_rev = function(fastqDataframe, fileName = NULL){
+  stopifnot(is.data.frame(fastqDataframe))
+  stopifnot(all(colnames(fastqDataframe) == c("label", "sequence", "mark", "score")))
+  stopifnot(nrow(fastqDataframe) > 1)
+
+  fastq = data.table(as.vector(t(fastqDataframe)))
+  if (!is.null(fileName)){
+    if(length(grep(".+\\.fastq$", fileName, ignore.case = T)) == 0){
+      fileName = paste0(fileName, ".fastq")
+    }
+
+    fwrite(fastq, file = fileName, col.names = F, quote = F, showProgress = T)
+  }
+  return(fastq)
+}
+
