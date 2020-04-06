9bbd03a54f8004fabc5c9a2601f6360385f0564c
paodan <weiyangtao1513@gmail.com>
2020-04-07 01:43:30 +0200

    replace where2SearchFun by whereis

--- R/where2SearchFun.R
+++ R/where2SearchFun.R
@@ -5,35 +5,40 @@
 #' environment, the parent environment is further searched until the top 
 #' environment, i.e. empty environment (\code{emptyenv()}) 
 #' or \code{R_EmptyEnv}.
-#' @return environment/package name(s) where the function x is found.
-#' R_GlobalEnv is the global environment (\code{globalenv()}). If 
-#' function x is not found, \code{NULL} is returned. If multiple 
+#' @param mode either "function" or "any", if mode = "function" (default),
+#' considering x is only a function object.
+#' @return environment where the function/object x is found.
+#' If x is not found, \code{NULL} is returned. If multiple 
 #' environments are found, the first environment is the default place to 
-#' call the function x.
+#' get the value of x.
 #' @export
 #' @examples 
 #' \dontrun{
 #' detachPackages("Matrix")
-#' where2SearchFun("print")
-#' where2SearchFun(print)
+#' whereis("print")
+#' whereis(print)
 #' 
 #' # Matrix::print is the default print
 #' library(Matrix)
-#' where2SearchFun("print")
+#' whereis("print")
 #' identical(print, Matrix::print)
 #' identical(print, base::print)
 #' 
 #' print = function() print()
-#' where2SearchFun("print")
+#' whereis("print")
+#' 
+#' # Search any object
+#' whereis(.Options, mode = "any")
 #' }
-where2SearchFun = function(x, env = parent.frame()) {
+whereis = function(x, env = parent.frame(), mode = c("function", "any")) {
+  mode = match.arg(mode)
   if (tryCatch(!is.character(x), error = function(e) TRUE)) 
     x = as.character(substitute(x))
   
   res = c()
   while(!identical(env, emptyenv())){
-    if (exists(x, envir = env, mode = "function", inherits = FALSE)) {
-      res = c(res, .rmpkg(environmentName(env)))
+    if (exists(x, envir = env, mode = mode, inherits = FALSE)) {
+      res = c(res, env)
     }
     env = parent.env(env)
   }
