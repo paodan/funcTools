8b21baf3732f0e61a760977cc00845c52ec596bf
Pao Dan <paodan@Paos-MacBook-Pro.local>
2020-04-15 01:41:18 +0200

    add %,% and %, % function

---
+++ R/paste2strings.R
@@ -0,0 +1,25 @@
+#' @title Paste two strings without a space
+#' 
+#' @return Paste two strings without a space
+#' @param x character
+#' @param y character
+#' @rdname Paste2strings
+#' @export
+"%,%" = function(x, y){
+  paste0(x, y)
+}
+
+#' @title Paste two strings with a space
+#' 
+#' @return Paste two strings with a space
+#' @param x character
+#' @param y character
+#' @rdname Paste2strings
+#' @export
+#' @examples {
+#' "a" %,% "b"
+#' "a" %, % "b"
+#' }
+"%, %" = function(x, y){
+  paste(x, y)
+}
