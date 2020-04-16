b1fe2cd897a52c2b810951ea66c89087ce210d73
Pao Dan <paodan@Paos-MacBook-Pro.local>
2020-04-16 12:17:38 +0200

    add %/% function

--- R/paste2strings.R
+++ R/paste2strings.R
@@ -19,7 +19,19 @@
 #' @examples {
 #' "a" %,% "b"
 #' "a" %, % "b"
+#' "folder" %/% "file"
 #' }
 "%, %" = function(x, y){
   paste(x, y)
+}
+
+#' @title Paste two strings with a space
+#' 
+#' @return Paste two strings with a slash (/)
+#' @param x character
+#' @param y character
+#' @rdname Paste2strings
+#' @export
+"%/%" = function(x, y){
+  paste0(x, "/", y)
 }
