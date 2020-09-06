7c85e4567ec204792f8c4a8cc0d124bd5187767c
paodan <weiyangtao1513@gmail.com>
2020-09-06 10:28:16 +0200

    add thisFile function

--- R/setwd2thisFile.R
+++ R/setwd2thisFile.R
@@ -1,4 +1,4 @@
-#' @title Set working directory according to the file including this function\cr
+#' @title Set working directory according to the file with this function\cr
 #' @description Set working directory to where the file that put this
 #' function located.
 #' @return the working directory before the change.
@@ -24,4 +24,26 @@ setwd2thisFile = function(){
     cat("Set working directory to", this.dir, "\n")
   }
   invisible(preDir)
+}
+
+
+#' @title The path, directory and file name of the current (executed) file
+#' @return a list of full path, directory and file name of the current 
+#' (executed) file.
+#' @import rstudioapi
+#' @export
+#' @examples {
+#' \dontrun{
+#' # Put the following codes in one file and save.
+#' thisFile()
+#' # Or run it in the R studio console.
+#' }
+#' }
+#' @export
+thisFile = function(){
+  res = list()
+  res$path = getActiveDocumentContext()$path
+  res$dir = dirname(res$path)
+  res$file = basename(res$path)
+  return(res)
 }
