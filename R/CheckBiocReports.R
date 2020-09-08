5a8375f1836ec944244896610b9b0a78c2240ee6
paodan <weiyangtao1513@gmail.com>
2020-09-08 17:40:06 +0200

    add CheckBiocReports function

---
+++ R/CheckBiocReports.R
@@ -0,0 +1,54 @@
+#' Check the package reports on Biocondocutor
+#' @param package the package name
+#' @param url0 the url of the latest bioconductor package reports.
+#' @importFrom xml2 read_html
+#' @importFrom XML htmlTreeParse xmlValue
+#' @export
+#' @examples 
+#' \dontrun{
+#' CheckBiocReports()
+#' CheckBiocReports("FlowSOM")
+#' }
+CheckBiocReports = function(package = "RegEnrich", 
+                            url0 = "http://bioconductor.org/checkResults/devel/bioc-LATEST/"){
+  url = paste0(url0, package)
+  x = read_html(url)
+  y = htmlTreeParse(x)
+  
+  tb = y$children$html[['body']][[6]]
+  
+  text = c()
+  for(mi in seq_along(tb)){
+    # print(tb[[mi]])
+    # Sys.sleep(2)
+    for (ni in seq_along(tb[[mi]])){
+      tmp = xmlValue(tb[[mi]][[ni]])
+      if (length(tmp) == 0) tmp = ""
+      
+      text = c(text, tmp)
+    }
+  }
+  res = rbind(matrix(head(text, 16), ncol = 8, byrow = T),
+              cbind(rep("", 3), 
+                    matrix(tail(text, -16), ncol = 7, byrow = T)))
+  info = res[1:2, 1]
+  # print(info)
+  res_colname = res[1, c(-1, -8)]
+  # print(res_colname)
+  res = data.frame(res[-1, c(-1, -8)])
+  colnames(res) = res_colname
+  cat(info, " ", sep = "\n")
+  
+  # Check warning and error
+  nError = sum(apply(res, 2, function(ii) substr(ii, 2, 6) == "ERROR" ))
+  nWarning = sum(apply(res, 2, function(ii) substr(ii, 2, 8) == "WARNING"))
+  if(nError > 0){
+    message(nError, " ERROR(s) exists.")
+  }
+  if(nWarning > 0){
+    message(nError, " WARNING(s) exists.")
+  }
+  
+  return(res)
+}
+
