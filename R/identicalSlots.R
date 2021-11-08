2278cb7baa8e761e2ab56b923be1f2b5df3cc7b0
paodan <weiyangtao1513@gmail.com>
2021-11-08 09:42:44 +0800

    add isList parameter to identicalSlots function

--- R/identicalSlots.R
+++ R/identicalSlots.R
@@ -3,6 +3,7 @@
 #' @param obj2 Second S4 object
 #' @param slots The slot names, the default is NULL, which means all of the slot
 #' names in the first object.
+#' @param isList logical, is the obj1 or obj2 a list, the default is FALSE.
 #' @return A list of two elements (FALSE and TRUE) if both exists. 
 #' The slot names in the FALSE element are not identical between two objects. 
 #' In contrast, the slot names in the TRUE element are identical between two 
@@ -20,17 +21,40 @@
 #' y = x + 1
 #' (B = sparseMatrix(i, j, x = y))
 #' identicalSlots(A, B)
+#' 
+#' x = list(a = 1:3, b = letters)
+#' y = list(a = 1:3, b = LETTERS)
+#' identicalSlots(x, y, isList = T)
+#' 
+#' x = list(a = 1:3, b = letters, c = TRUE)
+#' y = list(a = 1:3, b = LETTERS, c = TRUE)
+#' identicalSlots(x, y, isList = T)
 #' }
-identicalSlots = function(obj1, obj2, slots = NULL){
-  
+identicalSlots = function(obj1, obj2, slots = NULL, isList = FALSE){
+  gSlots = function(o, s){
+    if(is.list(o)){
+      y = o[[s]]
+    } else {
+      y = slot(o, s)
+    }
+    y
+  }
+  gSlotName = function(o){
+    if(is.list(o)){
+      y = names(o)
+    } else {
+      y = slotNames(o)
+    }
+    y
+  }
   if(is.null(slots)){
-    slots = slotNames(obj1)
+    slots = gSlotName(obj1)
   }
   
-  stopifnot(any(slots %in% slotNames(obj1)) && any(slots %in% slotNames(obj2)))
+  stopifnot(any(slots %in% gSlotName(obj1)) && any(slots %in% gSlotName(obj2)))
   slots = setNames(slots, slots)
   FT = unlist(lapply(slots, function(x) {
-    identical(slot(obj1, x), slot(obj2, x))
+    identical(gSlots(obj1, x), gSlots(obj2, x))
   }))
-  return(tapply(names(FT), FT, c))
+  return(tapply(names(FT), FT, list))
 }
