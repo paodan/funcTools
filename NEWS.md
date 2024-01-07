2f39cbb550a0259a416ba0f6ea1389d93ceec3c2
paodan <weiyangtao1513@gmail.com>
2024-01-07 12:24:07 +0800

    update createProject function

--- NEWS.md
+++ NEWS.md
@@ -1,4 +1,7 @@
-## funcTools 0.1.8
+## funcTools 0.1.10
+* Update createProject function
+
+## funcTools 0.1.9
 * Add runC function
 
 ## funcTools 0.1.8
--- R/createProject.R
+++ R/createProject.R
@@ -5,7 +5,11 @@
 #' @param proj project name
 #' @param path the path where the project is created
 #' @param .git logical, whether to create a git repository, default is FALSE
-#' @param showWarnings logical, whether to show warnings when create the folders, default is FALSE
+#' @param showWarnings logical, whether to show warnings when create the folders, 
+#' the default is FALSE
+#' @param inheritProj the project folder to be inherited, default is NULL
+#' @param inheritFolders the folder in the inheritProj project to be copied, 
+#' the default is c("Rscript")
 #' @return a list of two elements, i.e., `dirs` and `files`. `dirs` contains all
 #' the folders that have been created. `files` contains all the files that have
 #' been created.
@@ -24,24 +28,48 @@
 #' createProject("tempProj3", path = "~/", .git = TRUE)
 #' dir.exists("~/tempProj3/.git")
 #' unlink("~/tempProj3",recursive = TRUE) # remove this folder
+#' 
+#' # Inherit from a project
+#' createProject("tempProj4")
+#' file.create("tempProj4/0_RawData/tmpdataFile.txt") # make a file in 0_RawData folder
+#' createProject("tempProj5", inheritProj = "tempProj4", inheritFolders = c("Rscript", "0_RawData"))
+#' unlink("tempProj4",recursive = TRUE) # remove this folder
+#' unlink("tempProj5",recursive = TRUE) # remove this folder
 #' }
 #' @export
-createProject = function(proj, path = "./", .git = FALSE, showWarnings = FALSE){
-  path = file.path(path, proj)
-  if(dir.exists(path)){
-    stop(path, " (", normalizePath(path), ") exists. Change another proj name.")
+createProject = function(proj, path = "./", .git = FALSE, showWarnings = FALSE, 
+                         inheritProj = NULL, inheritFolders = c("Rscript")){
+  path1 = file.path(path, proj)
+  if(dir.exists(path1)){
+    stop(path1, " (", normalizePath(path1), ") exists. Change another proj name.")
   }
-  dirs = paste0(path, c("", "/0_RawData", "/Rdata", "/Rscript", "/figs", "/csv", "/ppt"))
-  files = paste0(path, c("/Rscript/00_01_functions.R", "/Rscript/01_01_readData.R"))
+  
+  dirs = paste0(path1, c("", "/0_RawData", "/Rdata", "/Rscript", "/figs", "/csv", "/ppt"))
   for(mi in dirs){
     dir.create(mi, showWarnings = showWarnings, recursive = T)
   }
-  if(file.exists(paste0(path, "/Rscript"))){
-    file.create(files)
+  
+  if(is.null(inheritProj)){
+    files = paste0(path1, c("/Rscript/00_01_functions.R", "/Rscript/01_01_readData.R"))
+    if(file.exists(paste0(path1, "/Rscript"))){
+      file.create(files)
+    }
+    
+  } else if (!is.null(inheritProj) && dir.exists(file.path(inheritProj, inheritFolders))){
+    files = c()
+    for(mi in seq_along(inheritFolders)){
+      from = file.path(inheritProj, inheritFolders)[mi]
+      files0 = dir(from, all.files = T, full.names = T, include.dirs = T, no.. = T)
+      folder1 = file.path(path, proj, inheritFolders[mi])
+      file.copy(files0, to = folder1, recursive = T)
+      files = c(files, file.path(folder1, basename(files0)))
+    }
+  } else {
+    stop("inheritFolders does not exist.")
   }
   x = system("which git", intern = T)
   if(length(x) == 1 && .git){
-    cmd = paste("cd", path, "; git init")
+    cmd = paste("cd", path1, "; git init")
     system(cmd)
   }
   return(list(dirs = dirs, files = files))
